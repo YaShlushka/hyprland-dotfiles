1384b7ae04ec0e143ba8ce28c0eee11d916b6ab8
YaShlushka <paderinee@gmail.com>
2025-08-28 13:52:08 +0300

    some changes

--- install_packets.sh
+++ install_packets.sh
@@ -26,18 +26,18 @@ sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-libera
 sudo pacman -S --noconfirm hyprlock hyprshot
 
 #xdg-desktop
-sudo pacman -S --noconfirm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-utils
+sudo pacman -S --noconfirm xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-utils
 
 # applications
-sudo pacman -S --noconfirm firefox chromium btop steam gnome-calculator obs-studio mpv nvim viewnior thunar swaync
-yay -S --noconfirm minecraft-launcher
+sudo pacman -S --noconfirm rofi-wayland firefox chromium btop steam gnome-calculator obs-studio mpv nvim viewnior thunar swaync
+yay -S --noconfirm minecraft-launcher windscribe-v2-bin
 
 # decorative
 sudo pacman -S --noconfirm cava fastfetch swww imagemagick python-pywal papirus-icon-theme
 yay -S --noconfirm peaclock eww pipes.sh waybar-cava pywal-discord-git
 
 # utils
-sudo pacman -S --noconfirm brightnessctl clang gcc gdb cliphist flatpak gamemode lib32-gamemode pamixer os-prober gnome-keyring jdk21-openjdk
+sudo pacman -S --noconfirm brightnessctl clang gcc gdb cliphist flatpak gamemode lib32-gamemode pamixer os-prober gnome-keyring jdk21-openjdk zip unzip unrar thunar-archive-plugin bind
 
 # nvidia
 sudo pacman -S --noconfirm linux-headers nvidia nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings egl-wayland libva-nvidia-driver nvidia-prime
