1a07019a2109d7c47a8df0e1680289cfa655de9b
YaShlushka <paderinee@gmail.com>
2025-08-28 21:59:03 +0300

    Update install_packets.sh

--- install_packets.sh
+++ install_packets.sh
@@ -8,19 +8,19 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm git base-devel curl dnsutils npm
 
 # install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si --noconfirm
+# cd ..
+# rm -rf yay
 
 # install zsh
 sudo pacman -S --noconfirm zsh
-sudo chsh -s "$(which zsh)"
+# sudo chsh -s "$(which zsh)"
 npm install --global pure-prompt
 
 # fonts
-sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome otf-font-awesome
+sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd ttf-jetbrains-mono # ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome otf-font-awesome
 
 # hypr
 sudo pacman -S --noconfirm hyprlock hyprshot
@@ -30,11 +30,11 @@ sudo pacman -S --noconfirm xdg-desktop-portal xdg-desktop-portal-hyprland xdg-de
 
 # applications
 sudo pacman -S --noconfirm rofi-wayland firefox chromium btop steam gnome-calculator obs-studio mpv nvim viewnior thunar swaync
-yay -S --noconfirm minecraft-launcher windscribe-v2-bin
+# yay -S --noconfirm minecraft-launcher windscribe-v2-bin
 
 # decorative
 sudo pacman -S --noconfirm cava fastfetch swww imagemagick python-pywal papirus-icon-theme
-yay -S --noconfirm peaclock eww pipes.sh waybar-cava pywal-discord-git
+# yay -S --noconfirm peaclock eww pipes.sh waybar-cava pywal-discord-git
 
 # utils
 sudo pacman -S --noconfirm brightnessctl clang gcc gdb cliphist flatpak gamemode lib32-gamemode pamixer os-prober gnome-keyring jdk21-openjdk zip unzip unrar thunar-archive-plugin bind
@@ -46,4 +46,4 @@ sudo pacman -S --noconfirm linux-headers nvidia nvidia-dkms nvidia-utils lib32-n
 sudo pacman -S --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools
 
 #flatpak
-flatpak install -y --noninteractive flathub com.spotify.Client
+#flatpak install -y --noninteractive flathub com.spotify.Client
